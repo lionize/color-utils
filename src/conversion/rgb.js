4e3d2d482779f0eb4e10a8a7aa6174b53df7535a
Mark Chandler <mark.allen.chandler@gmail.com>
2017-08-27 20:29:10 -0400

    Add rgbObjectToHex

--- src/conversion/rgb.js
+++ src/conversion/rgb.js
@@ -3,12 +3,18 @@ import { rgbRegex } from '../regex'
 export const rgbStringToHex = input => {
   const match = rgbRegex.exec(input)
   let [r, g, b, a] = match.slice(1)
+  return rgbaToString(r, g, b, a)
+}
+
+export const rgbObjectToHex = ({ r, g, b, a }) => rgbaToString(r, g, b, a)
+
+const rgbaToString = (r, g, b, a) => {
   r = parseInt(r).toString(16)
   g = parseInt(g).toString(16)
   b = parseInt(b).toString(16)
   let string = `#${r}${g}${b}`
   if (a) {
-    a = parseInt(a * 255).toString(16)
+    a = (parseFloat(a).toFixed(2) * 255).toString(16).slice(0, 2)
     string += a
     if (a.length == 1) {
       string += a
--- src/conversion/rgb.spec.js
+++ src/conversion/rgb.spec.js
@@ -1,13 +1,17 @@
-import { rgbStringToHex } from './rgb'
+import { rgbStringToHex, rgbObjectToHex } from './rgb'
 
 test('rgbStringToHex', () => {
   const rgbStr = 'rgb(255, 255, 255)'
-  let expected = '#FFFFFF'
-
-  expect(rgbStringToHex(rgbStr)).toEqual(expected)
+  expect(rgbStringToHex(rgbStr)).toEqual('#FFFFFF')
 
   const rgbaStr = 'rgb(255, 255, 255, 1)'
-  expected = '#FFFFFFFF'
+  expect(rgbStringToHex(rgbaStr)).toEqual('#FFFFFFFF')
+})
 
-  expect(rgbStringToHex(rgbaStr)).toEqual(expected)
+test('rgbObjectToHex', () => {
+  const rgbObj = { r: 255, g: 255, b: 255 }
+  expect(rgbObjectToHex(rgbObj)).toEqual('#FFFFFF')
+
+  const rgbaObj = { r: 255, g: 255, b: 255, a: 0.5 }
+  expect(rgbObjectToHex(rgbaObj)).toEqual('#FFFFFF7F')
 })
